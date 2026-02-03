756ec24648bc5a150e6c3722fd489a98c7526ff2
Daniel <danielzmalka@gmail.com>
2026-02-03 13:15:19 +0200

    feat: add disk usage alert logic SCRUM-28 #in-progress #comment Threshold set to 85% for high usage alerts #time 45m

--- monitor.sh
+++ monitor.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+USAGE=$(df -h | grep "C:" | awk '{print $6}' | tr -d '%')
 LOG_FILE="system_monitor.log"
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
@@ -9,5 +10,10 @@ echo "Report generated at: $TIMESTAMP" >> $LOG_FILE
 echo "Memory Info:" >> $LOG_FILE
 cat /proc/meminfo | grep -E "MemTotal|MemFree" >> $LOG_FILE
 
-echo "Disk Usage:" >> $LOG_FILE
-df -h | grep "C:" >> $LOG_FILE
+echo "The disk usage is: $USAGE percent" >> $LOG_FILE
+
+if [ $USAGE -gt 85 ]; then
+    echo "WARNING: High disk usage detected: $USAGE%" >> $LOG_FILE
+else
+    echo "Status: Disk usage is normal." >> $LOG_FILE
+fi
